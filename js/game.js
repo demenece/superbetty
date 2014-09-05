acdc9eea64512d80e3e731c53aeb6a9dd892bf86
demenece <damiancalderon@gmail.com>
2014-09-05 21:30:40 +0000

    reorganizing code

--- js/game.js
+++ js/game.js
@@ -1,37 +1,50 @@
 $(document).ready(function(){
-  $(".coffee, .pie, .brownie").draggable({
+  
+  // Init
+  // ----------------------------
+  
+  // Hacer draggables los supplies
+  $(".supply").draggable({
     containment: "#game-container",
-    revert: 'invalid'
+    revert: 'invalid',
+    helper: function( event ) {
+      return $( "<div></div>").addClass($(this).attr("class") );
+    }
   });
+  
+  // Hacer dropeables los workers
   $(".worker").droppable( {
       hoverClass: 'hovered',
-      accept: '.coffee, .pie, .brownie',
       drop: function(event,ui){
         var supply = ui.draggable,
             me = $(this);
         
-        // Destruir elemento
-        supply.hide('slow');
-        
         // Asignar valor al worker
-        var currentVal = parseInt( me.text() );
+        var currentVal = parseInt( me.text(), 10 );
         me.text( currentVal + 1 );
         
-        // Crear nuevo supply
-        $(".supplies").append( $("<div></div>").addClass( 'supply ' + supply.attr('data-type') ) );
       }
   });
 
-  // var gameLoop = function() {
-  //   $('.worker').each(function() {
-  //     var currentHappines = parseInt($(this).attr('data-happines'), 10);
-  //     var random =  Math.round(Math.random() * 5)
-  //     $(this).attr('data-happines', currentHappines - random)
   
-  //     if (currentHappines - random <= 0) {
-  //       // Murio la cajita
-  //     }
-  //   });
-  // };
-  // setInterval(gameLoop, 50);
-});
+  
+  // Lógica del juego
+  // ----------------------------
+  
+  
+  var gameLoop = function() {
+    $('.worker').each(function() {
+      var currentStamina = parseInt($(this).attr('data-stamina'), 10);
+      var random =  Math.round(Math.random() * 5);
+      $(this).attr('data-stamina', currentStamina - random);
+  
+      if (currentStamina - random <= 0) {
+        // Murio la cajita
+      }
+    });
+  };
+  setInterval(gameLoop, 50);
+
+  
+  
+}); // Fin
